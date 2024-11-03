be04f0eb7a19073d4f3160be776774ffdb7b2bf9
samiullah <yarisamiullah@gmail.com>
2024-11-03 11:10:11 -0800

    first-one

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ResultsSum from "./results";
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+ <ResultsSum />
+)
